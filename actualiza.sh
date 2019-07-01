57c41037e0801211b21ca525b9614e11406f3a87
clonbg <clonbg@gmail.com>
2019-07-01 15:19:01 +0200

    trizen sin sudo

--- actualiza.sh
+++ actualiza.sh
@@ -1,7 +1,7 @@
 
 #!/bin/bash --login
 trizen -Syu --noconfirm --noedit
-sudo trizen -Rsn $(trizen -Qdtq) --noconfirm
-sudo trizen -Scc --noconfirm
+trizen -Rsn $(trizen -Qdtq) --noconfirm
+trizen -Scc --noconfirm
 echo "Problemas con las comprobaciones de integridad: "
 echo "yaourt --m-arg --skipchecksums --m-arg --skippgpcheck paquete"
