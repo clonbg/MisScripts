65cb20be959506fe230835c7839c0b73234179a0
Manuel Riquelme de la Paz <clonbg@gmail.com>
2019-08-30 13:51:57 +0200

    Update actualiza.sh

--- actualiza.sh
+++ actualiza.sh
@@ -4,4 +4,4 @@ trizen -Syu --noconfirm --noedit
 trizen -Rsn $(trizen -Qdtq) --noconfirm
 trizen -Scc --noconfirm
 echo "Problemas con las comprobaciones de integridad: "
-echo "yaourt --m-arg --skipchecksums --m-arg --skippgpcheck paquete"
+echo "trizen --skipinteg paquete"
