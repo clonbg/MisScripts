809d159048b8541791e9af6386366111a2d658f8
Manuel Riquelme de la Paz <clonbg@gmail.com>
2020-03-07 07:27:05 +0100

    reflector

--- actualiza.sh
+++ actualiza.sh
@@ -1,5 +1,6 @@
 
 #!/bin/bash --login
+sudo reflector --verbose --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
 trizen -Syu --noconfirm --noedit
 trizen -Rsn $(trizen -Qdtq) --noconfirm
 trizen -Scc --noconfirm
